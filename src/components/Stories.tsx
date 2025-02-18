4ab6ed68dfdaa75f09d8085eacefbd11454f647e
Moeed Rafi <moeedrafi95@gmail.com>
2025-02-18 18:32:05 +0500

    feat: Story Design

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
+
 const Stories = () => {
-  return <div>Stories</div>;
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide ">
+      <div className="flex gap-8 w-max">
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="/noCover.png"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Add a Story</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stories;
