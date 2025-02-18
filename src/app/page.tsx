6266bb250d5d9ef55dcbf1f82f529e6fc05e2620
Moeed Rafi <moeedrafi95@gmail.com>
2025-02-18 18:23:23 +0500

    feat: HomePage Design + Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import Feed from "@/components/feed/Feed";
+import AddPost from "@/components/AddPost";
+import Stories from "@/components/Stories";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex gap-6 pt-6">
+      {/* LEFT */}
+      <div className="hidden xl:block w-[20%]">Left</div>
+
+      {/* CENTER */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="hidden lg:block w-[30%]">right</div>
+    </div>
+  );
 }
